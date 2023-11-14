c86197c0f7e77ea55ab9b1f135281b818d10202b
Dezzelshipc <foler126@gmail.com>
2023-11-14 19:56:56 +1000

    s5-op-l2

--- sem5-optimize/lab2.py
+++ sem5-optimize/lab2.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 
+def f0(var_vec: np.ndarray, mat: np.matrix, b_vec: np.ndarray) -> int | float:
+    vv = var_vec[:-1]
+    out = vv.T.dot(mat).dot(vv) / 2 + vv.T.dot(b_vec)
+    return out.sum()
+
+
 def f(var_vec: np.ndarray, mat: np.matrix, b_vec: np.ndarray, center: np.ndarray, radius: float) -> int | float:
     n = len(mat)
     vv = var_vec[:n]
@@ -11,7 +17,7 @@ def f(var_vec: np.ndarray, mat: np.matrix, b_vec: np.ndarray, center: np.ndarray
 
 def f_grad(var_vec: np.ndarray, mat: np.matrix, b_vec: np.ndarray, center: np.ndarray) -> np.ndarray:
     n = len(mat)
-    vv = var_vec[:n, 0]
+    vv = var_vec[:n]
     return mat.dot(vv) + b_vec + 2 * var_vec[n, 0] * (vv - center)
 
 
@@ -24,8 +30,8 @@ def w_mat(vv: np.ndarray, mat: np.matrix, center: np.ndarray) -> np.matrix:
     W = np.zeros((n + 1, n + 1))
     W[:n, :n] = mat + 2 * vv[n, 0] * np.eye(n)
 
-    W[n, :n] = (2 * (vv[:n, 0] - center)).flatten()
-    W[:n, n] = (2 * (vv[:n, 0] - center)).flatten()
+    W[n, :n] = (2 * (vv[:-1] - center)).flatten()
+    W[:n, n] = (2 * (vv[:-1] - center)).flatten()
     W[n, n] = 0
     return W
 
@@ -42,8 +48,8 @@ def g_vec(vv: np.ndarray, mat: np.matrix, b_vec: np.ndarray, center: np.ndarray,
 #                [3, 4, -5, 1],
 #                [-2, -5, 3, -2],
 #                [0, 1, -2, 5]])
-# b = np.matrix([[1], [1], [1], [1]])
-# c = np.matrix([[1], [1], [1], [1]])
+# b = np.matrix([[1.], [1.], [1.], [1.]])
+# c = np.matrix([[1.], [1.], [1.], [1.]])
 # r = 2
 # l = 1
 
@@ -51,46 +57,57 @@ def g_vec(vv: np.ndarray, mat: np.matrix, b_vec: np.ndarray, center: np.ndarray,
 #                [2, -1, 1, 5],
 #                [2, 1, 4, 3],
 #                [3, 5, 3, 6]])
-
-# b = np.matrix([[2], [6], [5], [3]])
-# c = np.matrix([[5], [5], [5], [2]])
+#
+# b = np.matrix([2., 6., 5., 3.]).T
+# c = np.matrix([5., 5., 5., 2.]).T
 # r = 2
 # l = 1
 
-A = np.matrix([[8., 3., 6., 6.],
-               [3., 4., 5., 5.],
-               [6., 5., 4., 7.],
-               [6., 5., 7., 6.]])
-b = np.matrix([2., 6., 5., 3.]).T
-x0 = np.matrix([2., 3., 0., 0.]).T
-l = 1
+A = np.matrix([[5., 1., 0., 10.],
+               [1., 4., 2., 4.],
+               [0., 2., 1., 20.],
+               [10., 4., 20., 0.]])
+b = np.matrix([0., 1., 1., 1.]).T
+c = np.matrix([0., 0., 0., 0.]).T
+l = 5.
+r = 10
 
-c = np.matrix([5., 5., 5., 2.]).T
-r = 1
+# A = np.matrix([[8., 3., 6., 6.],
+#                [3., 4., 5., 5.],
+#                [6., 5., 4., 7.],
+#                [6., 5., 7., 6.]])
+# b = np.matrix([2., 6., 5., 3.]).T
+# l = 1.
+# c = np.matrix([5., 5., 5., 2.]).T
+# r = 1
 
 for i in range(len(A)):
     print(np.linalg.det(A[:i + 1, :i + 1]), end=' ')
 print()
 
-mins = []
-
 n = len(A)
-x = np.matrix([[0.]] * (n + 1))
 
 eps = 1e-10
 
-for i in range(n * 5):
-    k = 0
-    x[:n] = c.copy()
-    x[n] = l
-    if i % 5 == 2:
-        continue
-    x[i // 5] += r * (i - 2)
+xs = []
+for index in range(n):
+    for koef in [-1.5, -1, -0.5, 0.5, 1, 1.5]:
+        new_x = c.copy()
+        new_x[index] += koef * r
+        xs.append(new_x)
 
+# xs = [np.matrix([1,2,3,4]).T]
+mins = []
+x = np.matrix([[0.]] * (n + 1))
+for x_i in xs:
+    x[:n] = x_i
+    x[n] = l
+
+    k = 0
     while True:
         G = g_vec(x, A, b, c, r)
-        if max(abs(G)) < eps:
-            print((f(x, A, b, c, r), x.T, k))  # , g_func(x, c, r)
+        if max(abs(G)) < eps or k > 1000:
+            mins.append((f0(x, A, b), x.copy().T, k))  # , g_func(x, c, r)
 
             break
         k += 1
@@ -99,8 +116,20 @@ for i in range(n * 5):
         # print(np.round(W, 10))
         W_inv = np.linalg.inv(W)
         # print(np.round(W_inv, 10))
-        l_old = x[n, 0]
         x = x - W_inv.dot(G)
         # print(x.T, end=' ')
 
-# print(mins, sep='\n')
+print(*mins, sep='\n')
+print()
+
+# l = 0
+A_inv = np.linalg.inv(A)
+x = - A_inv.dot(b)
+x = np.append(np.array(x), [0]).T
+g_num = g_func(x, c, r)
+if g_num < 0:
+    print(f'При l = 0 решение подходит: {x = }, f0 = {f(x, A, b, c, r)}, g = {g_num}')
+else:
+    print(f'При l = 0 решение не подходит: {x = }, f0 = {f(x, A, b, c, r)}, g = {g_num}')
+
+print(f"MINMIN: {min(mins, key=lambda y: y[0])}")
