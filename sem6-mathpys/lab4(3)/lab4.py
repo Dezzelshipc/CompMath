2f9a39786046980a408cc8286f083a77d9e993bd
Dezzelshipc <foler126@gmail.com>
2024-05-23 20:47:23 +1000

    lab fix

--- sem6-mathpys/lab4(3)/lab4.py
+++ sem6-mathpys/lab4(3)/lab4.py
@@ -5,11 +5,11 @@ def u1(r, f):
     return 7/5 - 1/6 * np.sin(f) - 1/48 * np.cos(4 * f)
 
 def u2(r,f):
-    return (3 * np.log(5) - 5 * np.log(4))/(np.log(5) - np.log(4)) + 2/(np.log(5) - np.log(4)) * np.log(r) - 23/18 *r*np.sin(f) + 184/(9 * r) * np.sin(f)
+    return (3 * np.log(5) - 10 * np.log(4))/(2*(np.log(5) - np.log(4))) + 7/(2*(np.log(5) - np.log(4))) * np.log(r) - 23/18 *r*np.sin(f) + 184/(9 * r) * np.sin(f)
 
 
-rl = np.linspace(0, 2, 101)
-fl = np.linspace(0, 2 * np.pi, 101)
+rl = np.linspace(0, 2, 201)
+fl = np.linspace(0, 2 * np.pi, 201)
 
 rr, ff = np.meshgrid(rl, fl)
 
@@ -19,7 +19,7 @@ fig = plt.figure(figsize=[5,5])
 ax = fig.add_axes([0.1,0.1,0.8,0.8], polar=True)
 # ax.set_rlim(3.8, 5)
 
-ax.pcolormesh(ff, rr, uu)
+ax.pcolor(ff, rr, uu, rasterized=True)
 
 plt.savefig('./sem6-mathpys/lab4(3)/plot.pdf')
 plt.show()
